569d1fcc9706d2ca8e0292c64ca129ae5ec9b932
alibekleader <149788620+alibekleader@users.noreply.github.com>
2024-08-18 16:12:05 +0500

    commit izohi

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8e24aa',
+        facebook: '#3b5999',
+        twitter: '#55acee',
+        linkedin: '#0077b5',
+        instagram: '#e4405f',
+        whatsapp: '#25d366',
+        youtube: '#cd201f',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
